d63e0807f15dad3027ee5e4c0fa08d8be85eba25
Simeon Yordanov <lookmanche@gmail.com>
2023-02-06 15:16:56 +0200

    added logic for initialization of Header class in his constructor

--- SoftuniHTTPServer/SoftuniHTTPServer.HTTP/Header.cs
+++ SoftuniHTTPServer/SoftuniHTTPServer.HTTP/Header.cs
@@ -2,6 +2,14 @@
 {
     public class Header
     {
+        public Header(string headerLine)
+        {
+            var headerParts = headerLine.Split(new string[] { ": " }, 2, StringSplitOptions.None);
+
+            this.Name = headerParts[0];
+            this.Value = headerParts[1];
+        }
+
         public string Name { get; set; }
 
         public string Value { get; set; }
