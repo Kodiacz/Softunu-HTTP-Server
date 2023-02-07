6be4f0b1581d1126d1acbc5d528e20f625001679
Simeon Yordanov <lookmanche@gmail.com>
2023-02-07 11:01:00 +0200

    implemented constructor

--- SoftuniHTTPServer/SoftuniHTTPServer.HTTP/HttpResponse.cs
+++ SoftuniHTTPServer/SoftuniHTTPServer.HTTP/HttpResponse.cs
@@ -1,6 +1,52 @@
 ﻿namespace SoftuniHTTPServer.HTTP
 {
+    using System.Text;
+    using static SoftuniHTTPServer.HTTP.HttpConstants;
+
     public class HttpResponse
     {
+        public HttpResponse(string contentType, byte[] body, HttpStatusCode statusCode = HttpStatusCode.Ok)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            StatusCode = statusCode;
+            Body = body;
+            this.Headers = new List<Header>()
+            {
+                {new Header("Content-Type", contentType) },
+                {new Header("Content-Length", body.Length.ToString())},
+            };
+            this.Cookies = new List<Cookie>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public ICollection<Header> Headers { get; set; }
+
+        public ICollection<Cookie> Cookies { get; set; }
+
+        public byte[] Body { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder responseBuilder = new StringBuilder();
+            responseBuilder.Append($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}"+NewLine);
+            foreach (var header in this.Headers)
+            {
+                responseBuilder.Append(header.ToString() + NewLine);
+            }
+
+            foreach (var cookie in this.Cookies)
+            {
+                responseBuilder.Append("Set-Cookie: " + cookie.ToString() + NewLine);
+            }
+
+            responseBuilder.Append(NewLine);
+
+            return responseBuilder.ToString();
+        }
     }
 }
