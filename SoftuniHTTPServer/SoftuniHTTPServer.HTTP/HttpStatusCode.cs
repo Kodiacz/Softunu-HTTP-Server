5795a815b2ccf428c386ec0a2c097c56917f87fb
Simeon Yordanov <lookmanche@gmail.com>
2023-02-07 11:01:49 +0200

    minor commit

---
+++ SoftuniHTTPServer/SoftuniHTTPServer.HTTP/HttpStatusCode.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftuniHTTPServer.HTTP
+{
+    public enum HttpStatusCode
+    {
+        Ok = 200,
+        MovedPermenantly = 301,
+        Found = 302,
+        TemporaryRedirect = 307,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
