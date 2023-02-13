6962b6cd817ad5f0872c0639b9558afc273de58b
Simeon Yordanov <lookmanche@gmail.com>
2023-02-13 10:14:56 +0200

    created a TestViewModel

---
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework.Tests/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftuniHTTPServer.MvcFramework.Tests
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
