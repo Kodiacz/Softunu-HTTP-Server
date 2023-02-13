5c60054e888d6e9dce1d4e4690149d5e771df7fc
Simeon Yordanov <lookmanche@gmail.com>
2023-02-13 10:11:05 +0200

    created a method that adds static file routes automatically

--- SoftuniHTTPServer/MvcApp/Startup.cs
+++ SoftuniHTTPServer/MvcApp/Startup.cs
@@ -18,20 +18,22 @@
             routeTable.Add(new Route("/Repositories/Create", HttpMethod.Get, new RepositoryController().Create));
             routeTable.Add(new Route("/Commits/Create", HttpMethod.Get, new CommitsController().Create));
             routeTable.Add(new Route("/Commits/All", HttpMethod.Get, new CommitsController().All));
-            routeTable.Add(new Route("/favicon.ico", HttpMethod.Get, new StaticFilesController().Favicon));
-            routeTable.Add(new Route("/css/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().CssBootStrapCss));
-            routeTable.Add(new Route("/css/bootstrap.min.js", HttpMethod.Get, new StaticFilesController().CssBootStrapJs));
-            routeTable.Add(new Route("/css/lazy.css", HttpMethod.Get, new StaticFilesController().LazyCss));
-            routeTable.Add(new Route("/css/demo.css", HttpMethod.Get, new StaticFilesController().DemoCss));
-            routeTable.Add(new Route("/css/rese-css.css", HttpMethod.Get, new StaticFilesController().ResetCss));
-            routeTable.Add(new Route("/css/site.css", HttpMethod.Get, new StaticFilesController().Site));
-            routeTable.Add(new Route("/css/text.css", HttpMethod.Get, new StaticFilesController().Text));
-            routeTable.Add(new Route("/js/lazy.js", HttpMethod.Get, new StaticFilesController().LazyJs));
-            routeTable.Add(new Route("/vendor/bootstrap/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().VendorBootStrapCss));
-            routeTable.Add(new Route("/vendor/bootstrap/bootstrap.min.js", HttpMethod.Get, new StaticFilesController().VendorBootStrapJs));
-            routeTable.Add(new Route("/vendor/jquery/jquery.min.js", HttpMethod.Get, new StaticFilesController().Jquery));
-            routeTable.Add(new Route("/vendor/popper/popper.min.js", HttpMethod.Get, new StaticFilesController().Popper));
-            routeTable.Add(new Route("/vendor/nouislider/js/nouislider.min.js", HttpMethod.Get, new StaticFilesController().Noislider));
+
+            // All Static Files:
+            //routeTable.Add(new Route("/favicon.ico", HttpMethod.Get, new StaticFilesController().Favicon));
+            //routeTable.Add(new Route("/css/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().CssBootStrapCss));
+            //routeTable.Add(new Route("/css/bootstrap.min.js", HttpMethod.Get, new StaticFilesController().CssBootStrapJs));
+            //routeTable.Add(new Route("/css/lazy.css", HttpMethod.Get, new StaticFilesController().LazyCss));
+            //routeTable.Add(new Route("/css/demo.css", HttpMethod.Get, new StaticFilesController().DemoCss));
+            //routeTable.Add(new Route("/css/rese-css.css", HttpMethod.Get, new StaticFilesController().ResetCss));
+            //routeTable.Add(new Route("/css/site.css", HttpMethod.Get, new StaticFilesController().Site));
+            //routeTable.Add(new Route("/css/text.css", HttpMethod.Get, new StaticFilesController().Text));
+            //routeTable.Add(new Route("/js/lazy.js", HttpMethod.Get, new StaticFilesController().LazyJs));
+            //routeTable.Add(new Route("/vendor/bootstrap/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().VendorBootStrapCss));
+            //routeTable.Add(new Route("/vendor/bootstrap/bootstrap.min.js", HttpMethod.Get, new StaticFilesController().VendorBootStrapJs));
+            //routeTable.Add(new Route("/vendor/jquery/jquery.min.js", HttpMethod.Get, new StaticFilesController().Jquery));
+            //routeTable.Add(new Route("/vendor/popper/popper.min.js", HttpMethod.Get, new StaticFilesController().Popper));
+            //routeTable.Add(new Route("/vendor/nouislider/js/nouislider.min.js", HttpMethod.Get, new StaticFilesController().Noislider));
         }
 
         public void ConfigureServices()
--- SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/Host.cs
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/Host.cs
@@ -1,4 +1,5 @@
 ﻿using SoftuniHTTPServer.HTTP;
+using HttpMethod = SoftuniHTTPServer.HTTP.HttpMethod;
 
 namespace SoftuniHTTPServer.MvcFramework
 {
@@ -8,11 +9,43 @@ namespace SoftuniHTTPServer.MvcFramework
         {
             // TODO: {controller}/{action}/{id}
             List<Route> routeTable = new List<Route>();
+            RegisterStaticFiles(routeTable);
             app.ConfigureServices();
             app.Configure(routeTable);
             IHttpServer server = new HttpServer(routeTable);
 
             await server.StartAsync(port);
+        }
+
+        private static void RegisterStaticFiles(List<Route> routeTable)
+        {
+            var staticFiles = Directory.GetFiles("wwwroot", "*", SearchOption.AllDirectories);
+            foreach (var file in staticFiles)
+            {
+                var url = file
+                    .Replace("wwwroot", string.Empty)
+                    .Replace("\\", "/");
+
+                routeTable.Add(new Route(url, HttpMethod.Get, (request) =>
+                {
+                    var fileContent = File.ReadAllBytes(file);
+                    var fileExt = new FileInfo(file).Extension;
+                    var contnentType = fileExt switch
+                    {
+                        ".txt" => "text/plain",
+                        ".js" => "text/javascript",
+                        ".css" => "text/css",
+                        ".jpg" => "image/jpg",
+                        ".jpeg" => "image/jpeg",
+                        ".png" => "image/png",
+                        ".gif" => "image/gif",
+                        ".ico" => "image/vnd.microsoft.icon",
+                        ".html" => "text/hml",
+                        _ => "text/plain",
+                    };
+                    return new HttpResponse(contnentType, fileContent, HttpStatusCode.Ok);
+                }));
+            }
         }
     }
 }
