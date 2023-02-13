e71d4b3b55529f1d7299ce342958974ba1f09e90
Simeon Yordanov <lookmanche@gmail.com>
2023-02-13 10:15:28 +0200

    working on created CRUD operation attributes

---
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/BaseHttpAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace SoftuniHTTPServer.MvcFramework
+{
+    using SoftuniHTTPServer.HTTP;
+
+    public abstract class BaseHttpAttribute : Attribute
+    {
+        public string Url { get; set; } = null!;
+
+        public abstract HttpMethod Method { get; }
+    }
+
+    public class HttpPostAttribute : BaseHttpAttribute
+    {
+        public HttpPostAttribute()
+        {
+        }
+
+        public HttpPostAttribute(string url)
+        {
+            this.Url = url;
+        }
+        
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public string Url { get; }
+    }
+    
+    public class HttpGetAttribute : BaseHttpAttribute
+    {
+        public HttpGetAttribute()
+        {
+        }
+
+        public HttpGetAttribute(string url)
+        {
+            Url = url;
+        }
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public string Url { get; }
+    }
+
+
+}
