4f527103ef0dd5469c5160e9a8eb3c6383b35069
Simeon Yordanov <lookmanche@gmail.com>
2023-02-14 11:13:39 +0200

    working on parsing the data from the post request body

---
+++ SoftuniHTTPServer/MvcApp/ViewModels/CreateRepositoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcApp.ViewModels
+{
+    public class CreateRepositoryViewModel
+    {
+        public string Name { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+    }
+}
