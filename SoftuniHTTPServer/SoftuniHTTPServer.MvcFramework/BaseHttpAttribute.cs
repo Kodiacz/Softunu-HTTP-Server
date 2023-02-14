5651f520af2e81038af1e2c5452f28b0a2c9222e
Simeon Yordanov <lookmanche@gmail.com>
2023-02-14 11:11:59 +0200

    extract the Get and Post attribute in separate file and made the Url prop abstact

--- SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/BaseHttpAttribute.cs
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/BaseHttpAttribute.cs
@@ -4,41 +4,8 @@
 
     public abstract class BaseHttpAttribute : Attribute
     {
-        public string Url { get; set; } = null!;
+        public abstract string Url { get; set; }
 
         public abstract HttpMethod Method { get; }
     }
-
-    public class HttpPostAttribute : BaseHttpAttribute
-    {
-        public HttpPostAttribute()
-        {
-        }
-
-        public HttpPostAttribute(string url)
-        {
-            this.Url = url;
-        }
-        
-        public override HttpMethod Method => HttpMethod.Post;
-
-        public string Url { get; }
-    }
-    
-    public class HttpGetAttribute : BaseHttpAttribute
-    {
-        public HttpGetAttribute()
-        {
-        }
-
-        public HttpGetAttribute(string url)
-        {
-            Url = url;
-        }
-        public override HttpMethod Method => HttpMethod.Get;
-
-        public string Url { get; }
-    }
-
-
 }
