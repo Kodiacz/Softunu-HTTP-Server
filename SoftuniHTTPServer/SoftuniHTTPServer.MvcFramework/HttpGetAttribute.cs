3d4f2ea455649f28fa680097ede48fd418e7184d
Simeon Yordanov <lookmanche@gmail.com>
2023-02-14 11:14:02 +0200

    moved files

---
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/HttpGetAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftuniHTTPServer.MvcFramework
+{
+    using SoftuniHTTPServer.HTTP;
+
+    public class HttpGetAttribute : BaseHttpAttribute
+    {
+        public HttpGetAttribute()
+        {
+        }
+
+        public HttpGetAttribute(string url)
+        {
+            this.Url = url;
+        }
+
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public override string Url { get; set; }
+    }
+
+
+}
---
+++ SoftuniHTTPServer/SoftuniHTTPServer.MvcFramework/HttpPostAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftuniHTTPServer.MvcFramework
+{
+    using SoftuniHTTPServer.HTTP;
+
+    public class HttpPostAttribute : BaseHttpAttribute
+    {
+        public HttpPostAttribute()
+        {
+        }
+
+        public HttpPostAttribute(string url)
+        {
+            this.Url = url;
+        }
+        
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public override string Url { get; set; }
+    }
+
+
+}
