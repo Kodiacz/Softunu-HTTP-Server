b9de48842b6eea5be02623d9f02f54ea91f8baff
Simeon Yordanov <lookmanche@gmail.com>
2023-02-16 10:45:17 +0200

    added services field and implemented Register, Login, Logout

--- SoftuniHTTPServer/MvcApp/Controllers/UsersController.cs
+++ SoftuniHTTPServer/MvcApp/Controllers/UsersController.cs
@@ -1,12 +1,22 @@
 ﻿namespace MvcApp.Controllers
 {
+    using MvcApp.Services;
     using SoftuniHTTPServer.HTTP;
     using SoftuniHTTPServer.MvcFramework;
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     public class UsersController : Controller
     {
+        private readonly UserServices userServices;
+
+        public UsersController()
+        {
+            this.userServices = new UserServices();
+        }
+
         [HttpGet]
         public HttpResponse Login()
         {
@@ -16,11 +26,18 @@
         [HttpPost("/Users/Login")]
         public HttpResponse DoLogin()
         {
-            var test = this.Request;
-            // TODO: READ DATA
-            // TODO: CHECK DATA
-            // TODO: LOG USER
-            // TODO: HOME PAGE
+            var username = this.Request.FormData["username"];
+            var password = this.Request.FormData["password"];
+            var userId = this.userServices.GetUserId(username, password);
+
+            if (userId == null)
+            {
+                // return error
+                return null;
+            }
+
+            this.SignIn((string)userId);
+
             return this.Redirect("/");
         }
 
@@ -30,5 +47,60 @@
             return View();
         }
 
+        [HttpPost("/Users/Register")]
+        public HttpResponse DoRegister()
+        {
+            var username = this.Request.FormData["username"];
+            var email = this.Request.FormData["email"];
+            var password = this.Request.FormData["password"];
+            var confirmPassword = this.Request.FormData["confirmPassword"];
+
+            //if (username == null || username.Length < 5 || username.Length > 20)
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            //if (!Regex.IsMatch(username, @"[a-zA-Z0-9\.]+"))
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            //if (string.IsNullOrEmpty(email) || !new EmailAddressAttribute().IsValid(email))
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            //if (password != confirmPassword)
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            //if (!this.userServices.IsUsernameAvailable(username))
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            //if (!this.userServices.IsEmailAvailable(email))
+            //{
+            //    // return error
+            //    return null;
+            //}
+
+            this.userServices.CreateUser(username, email, password);
+
+            return View(null, "Login");
+        }
+
+        [HttpGet]
+        public HttpResponse Logout()
+        {
+            this.SignOut();
+            return this.Redirect("/");
+        }
     }
 }
